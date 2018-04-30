a30fc4315cdbcf49f736717b5d7ad1b7629eb689
Grace Miller <gracemiller@Graces-MacBook-Pro-2.local>
2018-04-29 19:35:15 -0500

    added functions to clear search form, added css, added function for button to add 10 new gifs each time it's clicked, corrected broken pause/play function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
 var topics = ["Ali Wong", "Amy Schumer", "Kevin Hart", "Dave Chapelle", "Jim Gaffigan", "Daniel Tosh"];
 
 $(document).ready(function(){
+    $("#more-gifs").hide();
     createButtons();
     getGIFs();
-  
     newTopic();
-
 });
 
 
@@ -25,19 +24,23 @@ $("#buttons").on("click", "button", function () {
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div class='gif-div'>");
                 var rating = results[i].rating;
+                var title = results[i].title;
+                var heading = $("<h2>").text(title);
                 var p = $("<p>").text("Rating: " + rating);
                 var comedianGif = $("<img>").attr({
-                    "class": "gif",
                     "src": results[i].images.fixed_height_still.url,
+                    "class": "gif",
+                    "data-state":"still",
                     "data-still": results[i].images.fixed_height_still.url,
-                    "data-animate": results[i].images.fixed_height.url,
-                    "data-state":"still"
+                    "data-animate": results[i].images.fixed_height.url
                 });
 
-                $(gifDiv).append(comedianGif, rating);
+                $(gifDiv).append(heading, comedianGif, p);
                 $("#gifs").prepend(gifDiv);
-                pauseGifs();
+                $("#more-gifs").show();
             }
+            pauseGifs();
+            displayMore(comedian);
         });
     });
 }
@@ -50,7 +53,13 @@ function createButtons(){
         $(newButton).attr("data-person", topics[j]).text(topics[j]);
         $("#buttons").append(newButton);
     };
+    emptyForm();
 };
+
+//empty the search text box after the value is submitted
+function emptyForm(){
+    $("#new-topic").val("");
+}
 
 //function to add new entries to the topics array
 function newTopic(){
@@ -58,29 +67,67 @@ function newTopic(){
     $("#submit-btn").on("click", function(event){
        event.preventDefault();
        var topicAdd = $("#new-topic").val().trim();
-       console.log(topicAdd);
+       //prevent blank buttons from being created
+       if (topicAdd === ""){
+        return
+       }else{
        topics.push(topicAdd);
        createButtons();
-  
+        }
     });
 
 };
-
+//gifs load as still images, but animate or become still on click
 function pauseGifs(){
     
-    $(".gif").on("click", function(){
+    $(".gif-div").on("click", "img", function(){
         var state = $(this).attr("data-state");
         var animate = $(this).attr("data-animate");
         var still = $(this).attr("data-still");
         if ( state === "still"){
             $(this).attr("src", animate);
             $(this).attr("data-state", "animate");
-        }else{
+        }else {
             $(this).attr("src", still);
             $(this).attr("data-state", "still");
         }
     });
 }
 
+function displayMore(comedian){
+   
+    $("#more-gifs").on("click", function(){
+    
+        var clicks = $(this).attr("data-clicks")+1
+       var offset = clicks * 10;
 
-//api key JS5W7YN8LDrCq0DOfPlUINBtxyvdVVdj
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    comedian + "&offset="+ offset +"&limit=10&api_key=JS5W7YN8LDrCq0DOfPlUINBtxyvdVVdj";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+            var results = response.data;
+            console.log(queryURL);
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='gif-div'>");
+                var rating = results[i].rating;
+                var title = results[i].title;
+                var heading = $("<h2>").text(title);
+                var p = $("<p>").text("Rating: " + rating);
+                var comedianGif = $("<img>").attr({
+                    "src": results[i].images.fixed_height_still.url,
+                    "class": "gif",
+                    "data-state":"still",
+                    "data-still": results[i].images.fixed_height_still.url,
+                    "data-animate": results[i].images.fixed_height.url
+                });
+
+                $(gifDiv).append(heading, comedianGif, p);
+                $("#gifs").append(gifDiv);
+            }
+    });
+   
+    $(this).attr("data-clicks", clicks);
+});}
